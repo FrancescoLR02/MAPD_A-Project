da3d95b4ace5578ca46f7b24737d5362e7ba040f
boscolomarco <boscolomarco.ing@libero.it>
2024-12-23 23:38:54 +0100

    probably last version

--- RAM-programmer.ino
+++ RAM-programmer.ino
@@ -5,16 +5,10 @@
 
 //---------------------------------------------------------------------------------------------
 
-// Global variable to track if it is needed to write the RAM address
+// Global variable to be written inside an array which will have 
+// the info if the instuctions need to be written in the RAM address
 bool RAMwrite = true;
-// DA IMPLEMENTARE MEGLIO PERCHE' IN MANIERA GLOBALE DENTRO AL CICLO FOR PRINCIPALE
-// QUELLO DEI COMMANDS, NON FUNZIONEREBBE PERCHE' DEVE AD OGNI ITARAZIONE DIRE SE BISOGNA
-// SCRIVERE O MENO, MENTRE METTENDO I VALORI DELL'ARRAY ALL'INIZIO LI' SUBITO SI CAMBIA
-// IL VALORE DI QUESTA VARIABILE DI FLAG
-
-// forse soluzione è fare un array in cui con questa variabile true o false
-// quando si "scrive" il programma all'inizio si filli un array da 16 caselle
-// in cui si immagazzina di volta in volta true o false a seconda che dopo l'istruzione debba essere scritta o meno
+bool writeFlags[16];
 
 // Struct useful for the dictionary
 struct KeyValue {
@@ -90,26 +84,28 @@ byte ConvertSAPLine(const char *Instr = "", int Value = -1) {
   ConvertSAPLine("", -1)
 };*/
 
+byte data[16];
 
 // This program performs the calculation 6 + 10 - 5
-byte data[16] = {
-  data[0] = ConvertSAPLine(   "LDA", 15 ), // Load 6 (stored in RAM address 15)
-  data[1] = ConvertSAPLine(   "ADD", 14 ), // Add 10 (stored in RAM address 14)
-  data[2] = ConvertSAPLine(   "SUB", 13 ), // Subtract 5 (stored in RAM address 13)
-  data[3] = ConvertSAPLine(   "OUT", 0  ), // Output the result
-  data[4] = ConvertSAPLine(   "HLT", 0  ), // Halt the program
-  data[5]  = ConvertSAPLine(  "NOP", 0  ), // No operation
-  data[6]  = ConvertSAPLine(  ""   , 255 ),// just a test
-  data[7]  = ConvertSAPLine(  ""   , -1 ),
-  data[8]  = ConvertSAPLine(  ""   , -1 ),
-  data[9]  = ConvertSAPLine(  ""   , -1 ),
-  data[10]  = ConvertSAPLine( ""   , -1 ),
-  data[11]  = ConvertSAPLine( ""   , -1 ),
-  data[12]  = ConvertSAPLine( ""   , -1 ),
-  data[13] = ConvertSAPLine(  ""   , 5  ),
-  data[14] = ConvertSAPLine(  ""   , 10 ),
-  data[15] = ConvertSAPLine(  ""   , 6  ),
-};
+void initializeSAPProgram() {
+  data[0]  = ConvertSAPLine("LDA", 15);    writeFlags[0]  = RAMwrite;
+  data[1]  = ConvertSAPLine("ADD", 14);    writeFlags[1]  = RAMwrite;
+  data[2]  = ConvertSAPLine("SUB", 13);    writeFlags[2]  = RAMwrite;
+  data[3]  = ConvertSAPLine("OUT", 0);     writeFlags[3]  = RAMwrite;
+  data[4]  = ConvertSAPLine("HLT", 0);     writeFlags[4]  = RAMwrite;
+  data[5]  = ConvertSAPLine("NOP", 0);     writeFlags[5]  = RAMwrite;
+  data[6]  = ConvertSAPLine("", 255);      writeFlags[6]  = RAMwrite;
+  data[7]  = ConvertSAPLine("", -1);       writeFlags[7]  = RAMwrite;
+  data[8]  = ConvertSAPLine("", -1);       writeFlags[8]  = RAMwrite;
+  data[9]  = ConvertSAPLine("", -1);       writeFlags[9]  = RAMwrite;
+  data[10] = ConvertSAPLine("", -1);       writeFlags[10] = RAMwrite;
+  data[11] = ConvertSAPLine("", -1);       writeFlags[11] = RAMwrite;
+  data[12] = ConvertSAPLine("", -1);       writeFlags[12] = RAMwrite;
+  data[13] = ConvertSAPLine("", 5);        writeFlags[13] = RAMwrite;
+  data[14] = ConvertSAPLine("", 10);       writeFlags[14] = RAMwrite;
+  data[15] = ConvertSAPLine("", 6);        writeFlags[15] = RAMwrite;
+  return;
+}
 
 //---------------------------------------------------------------------------------------------
 
@@ -130,25 +126,29 @@ void setup() {
   pinMode(WRITE_EN, OUTPUT);
 
   digitalWrite(WRITE_EN, HIGH); // Set write button to HIGH initially (active low)
-  Serial.begin(57600);
+  //Serial.begin(57600);
+
+  initializeSAPProgram();
 
   for (int command = 0; command <= 15; command++) {
-    setLine(data[command]); // Set the RAM data/instruction
+    if (writeFlags[command]) { // Only write if the flag is true
+      setLine(data[command]); // Set the RAM data/instruction
 
-    int tempCommand = command;
+      int tempCommand = command;
 
-    // Set the RAM address using Arduino pins from 9 (LSB) to 12 (MSB)
-    for (int pin = 9; pin <= 12; pin++) {
-      digitalWrite(pin, tempCommand & 1); // Write each bit of the address
-      tempCommand = tempCommand >> 1;
+      // Set the RAM address using Arduino pins from 9 (LSB) to 12 (MSB)
+      for (int pin = 9; pin <= 12; pin++) {
+        digitalWrite(pin, tempCommand & 1); // Write each bit of the address
+        tempCommand = tempCommand >> 1;
+      }
+
+      // Simulate pressing the write button on the RAM
+      delay(500); // Wait for 0.5 second
+      digitalWrite(WRITE_EN, LOW);
+      delay(250); // Wait for 0.25 second
+      digitalWrite(WRITE_EN, HIGH);
+      delay(500); // Wait for 0.5 second
     }
-
-    // Simulate pressing the write button on the RAM
-    delay(500); // Wait for 0.5 second
-    digitalWrite(WRITE_EN, LOW);
-    delay(250); // Wait for 0.25 second
-    digitalWrite(WRITE_EN, HIGH);
-    delay(500); // Wait for 0.5 second
   }
 
   // Set everything to 0 at the end of the cycle
